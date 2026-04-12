034fe0cbbcfc03fae2d869f0db6730e0d4a4942d
Christian Langen <christian.langen@gmail.com>
2026-04-12 11:01:25 +0200

    dev

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,20 @@ use bevy::{
 };
 
 #[derive(Component, Default)]
-#[require(Transform)]
-struct Boulder {
-    velocity: f32,
-    mass: f32,
+#[require(Transform, Impulse)]
+struct RectShape {
     width: f32,
     height: f32,
 }
+
+#[derive(Component, Default)]
+struct Impulse {
+    mass: f32,
+    velocity: f32,
+}
+
+#[derive(Component)]
+struct Collidable();
 
 fn setup(
     mut commands: Commands,
@@ -30,29 +37,36 @@ fn setup(
         Mesh2d(my_shape.clone()),
         MeshMaterial2d(materials.add(Color::linear_rgb(255., 0., 0.))),
         Transform::from_xyz(0.0, 0.0, 0.0),
-        Boulder {
+        RectShape { width, height },
+        Impulse {
             velocity: 10.,
             mass: 1.,
-            width,
-            height,
         },
+        Collidable(),
     ));
 
     commands.spawn((
         Mesh2d(my_shape),
         MeshMaterial2d(materials.add(Color::linear_rgb(0., 255., 0.))),
         Transform::from_xyz(100.0, 0.0, 0.0),
-        Boulder {
+        RectShape { width, height },
+        Impulse {
             velocity: 10.,
             mass: 1.,
-            width,
-            height,
         },
+        Collidable(),
     ));
 
+    let wall_width = 100.;
+    let wall_height = 400.;
+
     commands.spawn((
+        RectShape {
+            width: wall_width,
+            height: wall_height,
+        },
         Sprite {
-            custom_size: Some(Vec2::new(100., 400.)),
+            custom_size: Some(Vec2::new(wall_width, wall_height)),
             image: asset_server.load("bricksx64.png"),
             image_mode: SpriteImageMode::Tiled {
                 tile_x: true,
@@ -62,25 +76,28 @@ fn setup(
             ..default()
         },
         Transform::from_xyz(-200.0, 0.0, 1.0),
+        Collidable(),
     ));
 }
 
-fn fixed_update(mut query: Query<(&mut Transform, &mut Boulder)>) {
-    for (mut transform, mut boulder) in &mut query {
-        transform.translation.x += boulder.velocity;
+fn fixed_update(mut query: Query<(&mut Transform, &mut Impulse)>) {
+    for (mut transform, mut impulse) in &mut query {
+        transform.translation.x += impulse.velocity;
         if transform.translation.x < -100. {
-            boulder.velocity *= -1.;
+            impulse.velocity *= -1.;
             transform.translation.x = -99.;
         }
         if transform.translation.x > 200. {
             transform.translation.x = 200.;
-            boulder.velocity *= -1.;
+            impulse.velocity *= -1.;
         }
     }
 }
 
-fn check_collisions(mut query: Query<(&Transform, &mut Boulder)>) {
-    fn bounding(t: &Transform, boulder: &Boulder) -> Aabb2d {
+fn check_collisions(
+    mut query: Query<(&Transform, &RectShape, Option<&mut Impulse>), With<Collidable>>,
+) {
+    fn bounding(t: &Transform, boulder: &RectShape) -> Aabb2d {
         let top_left = Vec2::new(
             t.translation.x - boulder.width / 2.,
             t.translation.y + boulder.height / 2.,
@@ -95,21 +112,35 @@ fn check_collisions(mut query: Query<(&Transform, &mut Boulder)>) {
         }
     }
     let mut iter = query.iter_combinations_mut();
-    while let Some([(transform, mut boulder), (transform2, mut boulder2)]) = iter.fetch_next() {
-        let one = bounding(transform, &boulder);
-        let two = bounding(transform2, &boulder2);
+    while let Some(
+        [
+            (transform, shape, mut impulse),
+            (transform2, shape2, mut impulse2),
+        ],
+    ) = iter.fetch_next()
+    {
+        let one = bounding(transform, &shape);
+        let two = bounding(transform2, &shape2);
         if !one.intersects(&two) {
             continue;
         }
-        let m1 = boulder.mass;
-        let m2 = boulder2.mass;
-        let v1 = boulder.velocity;
-        let v2 = boulder2.velocity;
+        match (impulse, impulse2) {
+            (Some(mut impulse), Some(mut impulse2)) => {
+                let m1 = impulse.mass;
+                let m2 = impulse2.mass;
+                let v1 = impulse.velocity;
+                let v2 = impulse2.velocity;
 
-        let new_v1 = (m1 * v1 + m2 * (2. * v2 - v1)) / (m1 + m2);
-        let new_v2 = (m2 * v2 + m1 * (2. * v1 - v2)) / (m1 + m2);
-        boulder.velocity = new_v1;
-        boulder2.velocity = new_v2;
+                let new_v1 = (m1 * v1 + m2 * (2. * v2 - v1)) / (m1 + m2);
+                let new_v2 = (m2 * v2 + m1 * (2. * v1 - v2)) / (m1 + m2);
+                impulse.velocity = new_v1;
+                impulse2.velocity = new_v2;
+            }
+            (Some(mut impulse), None) | (None, Some(mut impulse)) => {
+                impulse.velocity *= -1.;
+            }
+            _ => (),
+        }
     }
 }
 
