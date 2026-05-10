b37792f8d65274f5bee48088bf4b26735c80935a
Christian Langen <christian.langen@gmail.com>
2026-05-10 10:45:27 +0200

    dev

--- bouncing_ball/src/main.rs
+++ bouncing_ball/src/main.rs
@@ -10,28 +10,30 @@ fn main() {
 }
 
 fn setup(mut commands: Commands) {
-    // Ground: 600 pixels wide, 20 pixels high
-    commands.spawn((
-        RigidBody::Static,
-        Collider::rectangle(600.0, 20.0), // Match collider to sprite
-        Sprite {
-            color: Color::WHITE,
-            custom_size: Some(Vec2::new(600.0, 20.0)),
-            ..default()
-        },
-        Transform::from_xyz(0.0, -100.0, 0.0),
-    ));
-
     // Bouncing ball: 40 pixels diameter
     commands.spawn((
         RigidBody::Dynamic,
-        Collider::circle(20.0), // Radius 20 = Diameter 40
+        Collider::circle(20.0),
+        Restitution::new(0.8), // <--- Add this (0.0 = no bounce, 1.0 = perfect bounce)
         Sprite {
             color: Color::srgb_u8(255, 0, 0),
             custom_size: Some(Vec2::new(40.0, 40.0)),
             ..default()
         },
         Transform::from_xyz(0.0, 200.0, 0.0),
+    ));
+
+    // Ground
+    commands.spawn((
+        RigidBody::Static,
+        Collider::rectangle(600.0, 20.0),
+        Restitution::new(0.2), // Optional: adding some to the ground as well
+        Sprite {
+            color: Color::WHITE,
+            custom_size: Some(Vec2::new(600.0, 20.0)),
+            ..default()
+        },
+        Transform::from_xyz(0.0, -100.0, 0.0),
     ));
 
     // Camera
